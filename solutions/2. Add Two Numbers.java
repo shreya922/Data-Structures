21add6e70e2d5e91247988cb0c78e5e866af98ed
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-05-26 12:12:59 +0530

    1 ms

--- solutions/2. Add Two Numbers.java
+++ solutions/2. Add Two Numbers.java
@@ -10,22 +10,30 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode dummyHead = new ListNode(0);
-    ListNode p = l1, q = l2, curr = dummyHead;
-    int carry = 0;
-    while (p != null || q != null) {
-        int x = (p != null) ? p.val : 0;
-        int y = (q != null) ? q.val : 0;
-        int sum = carry + x + y;
-        carry = sum / 10;
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
-        if (p != null) p = p.next;
-        if (q != null) q = q.next;
-    }
-    if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
-    return dummyHead.next;
+    ListNode temp=new ListNode(0);
+        ListNode x1=temp;
+        int carry=0;
+        while(l1!=null||l2!=null)
+        {
+            int x=(l1!=null)?l1.val:0;
+            int y=(l2!=null)?l2.val:0;
+            int sum=x+y+carry;
+            ListNode curr=new ListNode(sum%10);
+            carry=sum/10;
+            temp.next=curr;
+            temp=temp.next;
+            if(l1!=null)
+                l1=l1.next;
+            if(l2!=null)
+                l2=l2.next;
+            
+        }
+        if(carry==1)
+        {
+           ListNode curr=new ListNode(carry); 
+            temp.next=curr;
+        }
+        return x1.next;
+        
 }
 }
