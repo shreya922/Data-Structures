2b04824ececcce4bded758b5b3df45b90c7f5fb2
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-01 22:43:11 +0530

    120 ms

--- solutions/227. Basic Calculator II.java
+++ solutions/227. Basic Calculator II.java
@@ -1,61 +1,24 @@
-                flag=true;
-                 while (!st.isEmpty() && prec(s.charAt(i)) 
-                         <=prec(st.peek())){
-                    
-                    res += st.pop()+" ";
-             }
-                st.push(s.charAt(i));
-            }
-            i++;
-        }
-    System.out.print(res);
-        if(flag==false)
+class Solution {
+    public int calculate(String s) {
+     Stack<Character> st=new Stack<Character>();
+        Boolean flag=false;
+        s="("+s+")";
+        s=s.replaceAll(" ","");
+        int num=0;
+        int i=0;
+             String[] tokens=new String[200000];
+        int k=0;   
+​
+        while(i<s.length())
         {
-            res=res.trim();
-            return Integer.parseInt(res);
-        }
-        String[] tokens=new String[res.length()];
-        int k=0;
-      
-          Stack<Integer> st1=new Stack<Integer>();
-          for(int j=0;j<res.length();j++)
-          {
-             tokens[k]="";
-              while(j<res.length()&&res.charAt(j)!=32)
-              {
-              tokens[k]=tokens[k]+String.valueOf(res.charAt(j));
-                  j++;
-              }
-                   //System.out.println(tokens[k]);
-              k++;
-          }
-        int a=0,b=0;
-        for( i=0;i<k;i++)
-        {
-            try{
-                st1.push(Integer.parseInt(tokens[i]));
-            }catch(Exception ex){
-           if(!st1.isEmpty())
-                 a=(int)st1.pop();
-                 if(!st1.isEmpty())
-                 b=(int)st1.pop();
-                if(tokens[i].equals("+"))
-                {
-                    st1.push(b+a);
-                }
-                else if(tokens[i].equals("*"))
-                    st1.push(b*a);
-                else if(tokens[i].equals("/"))
-                    st1.push(b/a);
-                else if(tokens[i].equals("-"))
-                    st1.push(b-a);
-            }
-        }
-        return st1.pop();
-    }
-    public int prec(char c)
-    {
-        switch(c)
-        {
-            case '+':
-            case '-':return 1;
+          
+           if(Character.isLetterOrDigit(s.charAt(i)))
+           {
+               num=0;
+               
+               while(i!=s.length()&&Character.isLetterOrDigit(s.charAt(i)))
+               {
+                  
+                   num=(num*10)+(int)(s.charAt(i)-48);
+                    i++;
+                   //System.out.println(num);
