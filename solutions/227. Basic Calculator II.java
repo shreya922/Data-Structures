ea966af95e5d72e4c9fb6dcb4d92ec69e18626f7
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-01 22:24:13 +0530

    N/A

--- solutions/227. Basic Calculator II.java
+++ solutions/227. Basic Calculator II.java
@@ -3,29 +3,29 @@ class Solution {
      Stack<Character> st=new Stack<Character>();
         Boolean flag=false;
         s="("+s+")";
-        //s=s.replaceAll(" ","");
+        s=s.replaceAll(" ","");
         String res="";
         int num=0;
         int i=0;
         //char[] ch=s.toCharArray();
         while(i<s.length())
         {
-            if(s.charAt(i)==' ')
-                continue;
+          
            if(Character.isLetterOrDigit(s.charAt(i)))
            {
                num=0;
-               i++;
+               
                while(i!=s.length()&&Character.isLetterOrDigit(s.charAt(i)))
                {
                   
-                   num=(num*10)+((int)(s.charAt(i)));
+                   num=(num*10)+(int)(s.charAt(i)-48);
                     i++;
-                   System.out.println(num);
+                   //System.out.println(num);
                }
+               
                i--;
                
-               res=res+String.valueOf(num);
+               res=res+String.valueOf(num)+" ";
            }
             else if(s.charAt(i)=='(')
                 st.push(s.charAt(i));
@@ -33,21 +33,45 @@ class Solution {
             {
                 while(!st.isEmpty()&&st.peek()!='(')
                 {
-                    res=res+st.pop();
+                    res=res+st.pop()+" ";
                 }
                 
+                if(!st.isEmpty())
                 st.pop();
             }
             else
             {
                 flag=true;
-                if(!st.isEmpty()&&prec(st.peek())>=prec(s.charAt(i)))
-                    st.push(s.charAt(i));
-                else
-                {
-                    while(!st.isEmpty()&&st.peek()!='(')
-                        res=res+st.pop();
-                    st.push(s.charAt(i));
-                }
+                 while (!st.isEmpty() && prec(s.charAt(i)) 
+                         <=prec(st.peek())){
+                    
+                    res += st.pop()+" ";
+             }
+                st.push(s.charAt(i));
             }
             i++;
+        }
+    System.out.print(res);
+        if(flag==false)
+        {
+            res=res.trim();
+            return Integer.parseInt(res);
+        }
+        String[] tokens=new String[res.length()];
+        int k=0;
+      
+          Stack<Integer> st1=new Stack<Integer>();
+          for(int j=0;j<res.length();j++)
+          {
+             tokens[k]="";
+              while(j<res.length()&&res.charAt(j)!=32)
+              {
+              tokens[k]=tokens[k]+String.valueOf(res.charAt(j));
+                  j++;
+              }
+                   //System.out.println(tokens[k]);
+              k++;
+          }
+        int a=0,b=0;
+        for( i=0;i<k;i++)
+        {
