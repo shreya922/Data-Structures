46228b7d0f467a004525986ecc832c992423a60f
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-08 21:12:53 +0530

    1 ms

--- solutions/232. Implement Queue using Stacks.java
+++ solutions/232. Implement Queue using Stacks.java
@@ -3,7 +3,7 @@ Stack<Integer> st1=new Stack<Integer>();
     Stack<Integer> st2=new Stack<Integer>();
     /** Initialize your data structure here. */
     public MyQueue() {
-        MyQueue myQueue = new MyQueue();
+        //MyQueue myQueue = new MyQueue();
     }
     
     /** Push element x to the back of queue. */
@@ -28,24 +28,3 @@ Stack<Integer> st1=new Stack<Integer>();
     
     /** Get the front element. */
     public int peek() {
-       if(!st1.isEmpty())
-        return st1.peek();
-        return -1;
-    }
-    
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        if(st1.size()==0)
-            return true;
-        return false;
-    }
-}
-​
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
