4779e7393db88a260eb91c4315aa618b9f53d267
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-11 14:06:07 +0530

    6 ms

--- solutions/503. Next Greater Element II.java
+++ solutions/503. Next Greater Element II.java
@@ -2,16 +2,10 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> st=new Stack<Integer>();
         int[] nextgreater=new int[nums.length];
-          int[] prevgreater=new int[nums.length];
-        HashMap<Integer,Boolean> map=new HashMap<Integer,Boolean>();
         for(int i=0;i<nums.length;i++)
-        {
+        {
            nextgreater[i]=Integer.MIN_VALUE; 
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-           prevgreater[i]=Integer.MIN_VALUE; 
-        }
+        }
         for(int i=0;i<nums.length;i++)
         {
             while(!st.isEmpty()&&nums[st.peek()]<nums[i])   
@@ -24,21 +18,22 @@ class Solution {
         st.clear();
         for(int i=nums.length-1;i>=0;i--)
         {
+            if(nextgreater[i]==Integer.MIN_VALUE)
+            {
             for(int j=0;j<i;j++)
             {
                 if(nums[j]>nums[i])
                 {
-                   prevgreater[i]=nums[j]; 
+                   nextgreater[i]=nums[j]; 
                     break;
                 }
+            }
             }
         }
         for(int i=0;i<nums.length;i++)
         {
-            if(nextgreater[i]==Integer.MIN_VALUE&&prevgreater[i]!=Integer.MIN_VALUE)
-              nextgreater[i]= prevgreater[i]; 
-            else if(nextgreater[i]==Integer.MIN_VALUE&&prevgreater[i]==Integer.MIN_VALUE)
-                nextgreater[i]=-1; 
+            if(nextgreater[i]==Integer.MIN_VALUE)
+              nextgreater[i]= -1; 
             
         }
         return nextgreater;
