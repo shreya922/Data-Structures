7b48668d8df28a00d7ac70d508b0c4d662c3df79
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-14 18:44:05 +0530

    26 ms

---
+++ solutions/402. Remove K Digits.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if(k==num.length())
+            return "0";
+        Stack<String> st=new Stack<String>();
+        int count=0;
+        for(int i=0;i<num.length();i++)
+        {
+            while(!st.isEmpty()&&Integer.parseInt(st.peek())>Integer.parseInt(String.valueOf(num.charAt(i)))&&count!=k)
+            {
+                st.pop();
+                count++;
+            }
+           // if(count<=k)
+            st.push(String.valueOf(num.charAt(i)));
+            
+        }
+        while(count!=k)
+        {
+         st.pop();
+            count++;
+        }
+        String x="";
+        while(!st.isEmpty())
+        {
+            
+            x=st.pop()+x;
+        }
+       StringBuilder sb=new StringBuilder(x);
+        int i=0;
+        while(sb.length()!=0&&sb.charAt(i)=='0'&&sb.length()!=1)
+        {
+            sb.deleteCharAt(i);
+        }
