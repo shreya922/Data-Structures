95db9f46b86b8a97bc4a600caa40291fd6500d99
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-14 10:38:48 +0530

    31 ms

--- solutions/739. Daily Temperatures.java
+++ solutions/739. Daily Temperatures.java
@@ -2,20 +2,14 @@ class Solution {
     public int[] dailyTemperatures(int[] a) {
         int[] ans=new int[a.length];
        Stack<Integer> st=new Stack<Integer>();
-        st.push(0);
-        for(int i=1;i<a.length;i++)
+        for(int i=0;i<a.length;i++)
         {
-            if(a[st.peek()]>a[i])
-                st.push(i);
-            else
-            {
                 while(!st.isEmpty()&&a[st.peek()]<a[i])
                 {
                     int a1=st.pop();
                     ans[a1]=i-a1;
                 }
                 st.push(i);
-            }
         }
         return ans;
         
