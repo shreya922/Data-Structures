453f8a9ae9d9109fe6360cdb970907c44ea0920d
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-19 12:31:31 +0530

    4 ms

---
+++ solutions/111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+       if(root.left==null&&root.right==null)
+           return 1;
+        else
+        {
+            int l=root.left!=null?minDepth(root.left):Integer.MAX_VALUE;
+            int r=root.right!=null?minDepth(root.right):Integer.MAX_VALUE;
+            return Math.min(l,r)+1;
+        }
+   }
+    
+}
