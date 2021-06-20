1b4a1893d2c1beb1c1ac8b0e887cbf14e62e8aca
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-06-20 11:29:26 +0530

    1 ms

--- solutions/1008. Construct Binary Search Tree from Preorder Traversal.java
+++ solutions/1008. Construct Binary Search Tree from Preorder Traversal.java
@@ -1,3 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -10,25 +19,30 @@ class Solution {
     }
     public TreeNode insert(int[] preorder)
     {
-        TreeNode root=null;
-        for(int i=0;i<preorder.length;i++)
+        TreeNode root=new TreeNode(preorder[0]);
+        Stack<TreeNode> st=new Stack<TreeNode>();
+        st.push(root);
+        for(int i=1;i<preorder.length;i++)
         {
-          root=createTree(root,preorder[i]);
+            int data=preorder[i];
+           if(!st.isEmpty()&&st.peek().val>data)
+        {
+            st.peek().left=new TreeNode(data);
+            st.push(st.peek().left);
+        }
+        else
+        {
+            TreeNode temp=null;
+            while(!st.isEmpty()&&st.peek().val<data)
+            {
+                temp=st.pop();
+            }
+            temp.right=new TreeNode(data);
+            st.push(temp.right);
+        }
         }
         return root;
     }
-    public TreeNode createTree(TreeNode root,int data)
-    {
-        if(root==null)
-        {
-            root=new TreeNode(data);
-            return root;
-        }
-        if(root.val<data)
-            root.right=createTree(root.right,data);
-         if(root.val>data)
-            root.left=createTree(root.left,data);
-        return root;
-    }
+    
    
 }
