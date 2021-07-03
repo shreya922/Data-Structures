e975c4cc543bcabd296f5c34fc5293528d1286a2
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-03 12:47:44 +0530

    1 ms

---
+++ solutions/110. Balanced Binary Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        int ldepth=maxdepth(root.left);
+        int rdepth=maxdepth(root.right);
+        if(Math.abs(ldepth-rdepth)>1)
+            return false;
+        return isBalanced(root.left)&&isBalanced(root.right);
+    }
+    public int maxdepth(TreeNode root)
+    {
+        int h=(root==null)?0:1+Math.max(maxdepth(root.left),maxdepth(root.right));
+        return h;
+    }
+    
+    
+}
