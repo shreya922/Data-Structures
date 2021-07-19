e35d4386fef4da6e2e3543b291f518153360dad5
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-19 12:01:35 +0530

    44 ms

---
+++ solutions/919. Complete Binary Tree Inserter.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class CBTInserter {
+TreeNode head=null;
+    public CBTInserter(TreeNode root) {
+        head=root;
+    }
+    
+    public int insert(int v) {
+        TreeNode newnode= insertNode(head,v);
+        return newnode.val;
+    }
+    public TreeNode insertNode(TreeNode root,int v)
+    {
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.add(root);
+           if(root==null)
+            return new TreeNode(v);
+        while(!q.isEmpty())
+        {
+            int x=q.size();
+            for(int i=0;i<x;i++)
+            {
+            TreeNode temp=q.remove();
+            if(temp.left!=null)
+            q.add(temp.left);
+                else
+                {
+                    temp.left=new TreeNode(v);
+                    return temp;
+                }
+             if(temp.right!=null)
+            q.add(temp.right);
+                 else
+                {
+                    temp.right=new TreeNode(v);
+                    return temp;
+                }
+            }
+        }
+        return root;
+        
+    }
+    public TreeNode get_root() {
+        return head;
+    }
+}
+​
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter obj = new CBTInserter(root);
