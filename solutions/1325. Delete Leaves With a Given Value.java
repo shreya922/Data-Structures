417a6043d242852aa95780730dd7fe1ec46ed4b9
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-21 16:07:50 +0530

    0 ms

--- solutions/1325. Delete Leaves With a Given Value.java
+++ solutions/1325. Delete Leaves With a Given Value.java
@@ -15,20 +15,17 @@
  */
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-      return wrapper(root,target);
-    }
-    public TreeNode wrapper(TreeNode root,int target)
-    {
-      if(root==null)
-          return null;
-         root.left=wrapper(root.left,target);
-        root.right=wrapper(root.right,target);
-      if(root.val==target&&root.left==null&&root.right==null)
-          root=null;
-       
+    if(root==null)
+        return null;
+         root.left= removeLeafNodes(root.left,target);
+     root.right=removeLeafNodes(root.right,target);  
+        if(root.left==null&&root.right==null&&root.val==target)
+        {
+            root=null;
+            return root;
+        }
+  
         return root;
-    }
-       
-    
    
+}
 }
