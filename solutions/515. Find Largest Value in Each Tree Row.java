6f78b6c6ba2c2588bb9b40e0ef458eee4043be9e
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-21 23:11:36 +0530

    2 ms

--- solutions/515. Find Largest Value in Each Tree Row.java
+++ solutions/515. Find Largest Value in Each Tree Row.java
@@ -1,3 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -8,34 +17,24 @@ class Solution {
     public List<Integer> largestValues(TreeNode root) {
          Queue<TreeNode> q=new LinkedList<TreeNode>();
         q.add(root);
-         List<List<Integer>> list1=new ArrayList<List<Integer>>();
-        List<Integer> d=new ArrayList<Integer>();
+        List<Integer> f=new ArrayList<Integer>();
            if(root==null)
-            return d;
+            return f;
         while(!q.isEmpty())
         {
             List<Integer> list=new ArrayList<Integer>();
             int x=q.size();
+            int max=Integer.MIN_VALUE;
             for(int i=0;i<x;i++)
             {
             TreeNode temp=q.remove();
+                if(temp.val>max)
+                    max=temp.val;
             list.add(temp.val);   
             if(temp.left!=null)
             q.add(temp.left);
              if(temp.right!=null)
             q.add(temp.right);
-            }
-            list1.add(list);
-        }
-        List<Integer> f=new ArrayList<Integer>();
-        for(int i=0;i<list1.size();i++)
-        {
-            int max=Integer.MIN_VALUE;
-            List<Integer> temp=list1.get(i);
-            for(int j=0;j<temp.size();j++)
-            {
-               if(temp.get(j)>max) 
-                   max=temp.get(j);
             }
             f.add(max);
         }
