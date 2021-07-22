3c3bb3f2a0aee54143f3270ba0621f4808fc1887
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-22 17:47:39 +0530

    6 ms

--- solutions/1372. Longest ZigZag Path in a Binary Tree.java
+++ solutions/1372. Longest ZigZag Path in a Binary Tree.java
@@ -1,8 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
@@ -14,34 +9,35 @@
  * }
  */
 class Solution {
-    int c=1;
+    
     int max=0;
     public int longestZigZag(TreeNode root) {
         if(root==null)
             return 0;
-       if(root!=null)
-       {
            findlongest1(root.left,"left",1);
             findlongest1(root.right,"right",1);
-           
-       }
         return max;
     }
-    public void findlongest1(TreeNode root,String direction,int step)
+    public void findlongest1(TreeNode root,String direction,int c)
     {
         if(root==null)
             return;
         if(direction.equals("left"))
         {
-       findlongest1(root.right,"right",step+1);
+        findlongest1(root.right,"right",c+1);
+            max=Math.max(max,c);
            findlongest1(root.left,"left",1);
+            
         }
         else
         {
-       findlongest1(root.left,"left",step+1);
-             findlongest1(root.right,"right",1);
+            findlongest1(root.left,"left",c+1);
+            max=Math.max(max,c);
+           findlongest1(root.right,"right",1);
+            max=Math.max(max,c);
+         
         }
-        max=Math.max(max,step);
+        
         
     }
      
