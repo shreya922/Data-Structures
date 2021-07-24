9a54c6aade38324b564425a58ce9447064de25e0
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-24 12:23:04 +0530

    0 ms

--- solutions/101. Symmetric Tree.java
+++ solutions/101. Symmetric Tree.java
@@ -1,64 +1,30 @@
-        {
-            temp=temp.left;
-            leftz++;
-        }
-        temp=root;
-        while(temp!=null)
-        {
-            temp=temp.right;
-            rightz++;
-        }
-        if(leftz!=rightz)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+return check(root.left,root.right);
+}
+    public Boolean check(TreeNode root1,TreeNode root2)
+    {
+        if(root1==null&&root2==null)
+            return true;
+        if(root1==null&&root2!=null||root1!=null&&root2==null)
             return false;
-       List<Integer> list1=new ArrayList<Integer>(); 
-         List<Integer> list2=new ArrayList<Integer>(); 
-        list1=found(root,list1);
-         list2=foundz(root,list2);
-        System.out.print(list1+" "+list2);
-        return list1.equals(list2);
-    }
-    public List<Integer> found(TreeNode root,List<Integer> list)
-    {
-         if(root.left!=null||root.right!=null)
-       {
-           
-             if(root.left!=null)
-             found(root.left,list);
-             else
-                 list.add(null);
-            // if(root.left==null)
-             list.add(root.val);
-             if(root.right!=null)
-             found(root.right,list);
-             else
-                 list.add(null);
-       }
-        if(root.left==null&&root.right==null)
-        {
-        list.add(root.val);
-        }
-        return list;
-    }
-    public List<Integer> foundz(TreeNode root,List<Integer> list)
-    {
-         if(root.left!=null||root.right!=null)
-       {
-           
-             if(root.right!=null)
-             foundz(root.right,list);
-             else
-                 list.add(null);
-            // if(root.left==null)
-             list.add(root.val);
-             if(root.left!=null)
-             foundz(root.left,list);
-             else
-                 list.add(null);
-       }
-        if(root.left==null&&root.right==null)
-        {
-        list.add(root.val);
-        }
-        return list;
+        if(root1.val!=root2.val)
+            return false;
+        return check(root1.left,root2.right)&&check(root1.right,root2.left);
     }
 }
