8abd770c24e3bb050ffbf625db60330d44996bf5
shreyatyagi-source <84306095+shreyatyagi-source@users.noreply.github.com>
2021-07-24 10:57:08 +0530

    22 ms

--- solutions/563. Binary Tree Tilt.java
+++ solutions/563. Binary Tree Tilt.java
@@ -17,41 +17,17 @@ class Solution {
     int leftsum=0;
     int rightsum=0;
     public int findTilt(TreeNode root) {
-         int x=0;
-        root=tempf(root);
-        List<Integer> list=new ArrayList<Integer>();
-        list=inorder(root,list);
-        System.out.print(list);
-        for(int i=0;i<list.size();i++)
-            x=x+list.get(i);
-        return x;
-    }
-    public TreeNode tempf(TreeNode root)
-    {
-      if(root==null)
-            return null;
+        if(root==null)
+            return 0;
         root.val=Math.abs(wrapper(root.left,0)-wrapper(root.right,0));
-        tempf(root.left);
-        tempf(root.right);
-        return root;
-    }
-     List<Integer> inorder(TreeNode root,List<Integer> list)
-    {
-         
-        if(root!=null)
-        {
-          inorder(root.left,list);
-            list.add(root.val);
-            inorder(root.right,list);
-        }
-         return list;
+        findTilt(root.left);
+        findTilt(root.right);
+        return wrapper(root,0);
     }
     public int wrapper(TreeNode root,int sum)
     {
        if(root==null)
-           return 0;
-        sum=sum+root.val+wrapper(root.left,sum)+wrapper(root.right,sum);
-       
+           return 0; sum=root.val+wrapper(root.left,sum)+wrapper(root.right,sum);
         return sum; 
         
     }
